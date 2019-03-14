d00effd73e814cd6bfdf697620c328b5b9e34278
yuehtzuwang <yuehtzuwang@gmail.com>
2019-03-14 19:21:23 +0800

    Update Settings.cs

--- app/XamarinClient/XamarinClient/Settings.cs
+++ app/XamarinClient/XamarinClient/Settings.cs
@@ -10,7 +10,7 @@ namespace XamarinClient
         public static string AppServiceURL = ""; //opc vault service url, ex: https://opcvault-service.azurewebsites.net
         public static string clientId = ""; //opcvault-module application id
         public static string commonAuthority = "https://login.microsoftonline.com/";
-        public static Uri returnUri = new Uri("urn:ietf:wg:oauth:2.0:oob"); //opcvault-module reply URLs
+        public static Uri returnUri = new Uri(""); //opcvault-module reply URLs
         public const string graphResourceUri = ""; //opcvault-service applicaiton id
         public const string ClientSecret = ""; //opcvault-module keys 
      }
